3819084b27f30286c764a6c702d6a34a4c48d742
Agustin Maciá <amaciadelrio@gmail.com>
2020-10-01 20:36:28 -0300

    PushReplacementNamed
    Sirve para cambiar la raiz. Una vez que me redirecciona a esa pagina no puedo volver atras.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:preferencias_usuario_app/src/pages/settings_page.dart';
 
 class HomePage extends StatelessWidget {
   
@@ -10,6 +11,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Preferencias del usuario'),
       ),
+      drawer: _crearMenu(context),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -19,6 +21,49 @@ class HomePage extends StatelessWidget {
           Divider(),
           Text('Nombre usuario:'),
           Divider(),
+        ],
+      ),
+    );
+  }
+
+  Drawer _crearMenu(BuildContext context) {
+    //Icono de hamburguesa para desplegar menu lateral
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/menu-img.jpg'),
+                fit: BoxFit.cover,
+              )
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home, color: Colors.blue),
+            title: Text('Inicio'),
+            onTap: () => Navigator.pushReplacementNamed(context, HomePage.routeName),
+          ),
+          ListTile(
+            leading: Icon(Icons.party_mode, color: Colors.blue),
+            title: Text('Fotos'),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.people, color: Colors.blue),
+            title: Text('Personas'),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings, color: Colors.blue),
+            title: Text('Ajustes'),
+            onTap: () {
+              //Cambiar raiz.
+              Navigator.pushReplacementNamed(context, SettingsPage.routeName);
+            }
+          ),
         ],
       ),
     );
