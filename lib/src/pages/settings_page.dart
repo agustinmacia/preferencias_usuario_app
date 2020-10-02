f08fe5c9f92fc0f7fe88d627faf7c8d3ae4b4527
Agustin Maciá <amaciadelrio@gmail.com>
2020-10-02 15:14:24 -0300

    Retoques en SettingsPage

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -10,6 +10,19 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  
+  bool _colorSecundario = true;
+  int _genero = 1;
+  String _nombre = 'Agustin'; 
+
+  TextEditingController _textController;
+
+  @override
+  void initState() { 
+    super.initState();
+      _textController = new TextEditingController(text: _nombre);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,26 +38,39 @@ class _SettingsPageState extends State<SettingsPage> {
           ),
           Divider(),
           SwitchListTile(
-            value: true,
+            value: _colorSecundario,
             title: Text('Color Secundario'),
-            onChanged: (valor){},
+            onChanged: (valor){
+              setState(() {
+                _colorSecundario = valor;
+              });
+            },
           ),
           RadioListTile(
             value: 1,
             title: Text('Masculino'),
-            groupValue: 1,
-            onChanged: (valor){},
+            groupValue: _genero,
+            onChanged: (valor){
+              setState(() {
+                _genero = valor;
+              });
+            },
           ),
           RadioListTile(
-            value: 1,
+            value: 2,
             title: Text('Femenino'),
-            groupValue: 2,
-            onChanged: (valor){},
+            groupValue: _genero,
+            onChanged: (valor){
+              setState(() {
+                _genero = valor;
+              });
+            },
           ),
           Divider(),
           Container(
             padding: EdgeInsets.symmetric(horizontal: 20.0),
             child: TextField(
+              controller: _textController,
               decoration: InputDecoration(
                 labelText: 'Nombre',
                 helperText: 'Nombre de la persona usando el telefono'
